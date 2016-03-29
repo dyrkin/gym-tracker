6341667281e73967ca42f77d2fff01dd6ffa1ae0
Eugene Zadyra <untrance@gmail.com>
2016-03-29 18:59:45 +0200

    Refactored userService

--- gym-tracker-core/src/main/scala/com/dyrkin/tracker/core/vo/VO.scala
+++ gym-tracker-core/src/main/scala/com/dyrkin/tracker/core/vo/VO.scala
@@ -10,6 +10,10 @@ case class Day(day:Int)
 case class Workout(id: Long, name: String, exercises: Seq[Exercise], days: Seq[Day])
 case class Program(id: Long, name: String, workouts:Seq[Workout])
 case class Value(value: Int)
+
+object WatchUserDetails {
+  def apply(user: (Long, String, String, String)) = WatchUserDetails(user._1, user._2, user._3, user._4)
+}
 case class WatchUserDetails(id: Long, name: String, email: String, uuid: String)
 
 
